80b8a28f2f52d506f535426e357a83285aa18d79
Parsa <mehdipourparsa@gmail.com>
2024-08-20 19:25:31 -0400

    feat : add Implementation

---
+++ DemoLibrary/Implementations/IPaymentGateway.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoLibrary.Implementations;
+
+/// <summary>
+/// The Implementation defines the interface for all implementation classes.
+/// It doesn't have to match the Abstraction's interface. In fact, the two
+/// interfaces can be entirely different. Typically the Implementation
+/// interface provides only primitive operations, while the Abstraction
+/// defines higher-level operations based on those primitives.
+/// This interface defines the payment gateway operations.
+/// </summary>
+public interface IPaymentGateway
+{
+    void AuthorizePayment(decimal amount, string data);
+    void CapturePayment();
+}
