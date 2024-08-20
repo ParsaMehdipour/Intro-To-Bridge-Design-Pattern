24b41f7beb8a1886eb64f2edc437444d67fcd6f5
Parsa <mehdipourparsa@gmail.com>
2024-08-20 19:25:19 -0400

    feat : add Abstractions

---
+++ DemoLibrary/Abstractions/PaymentProcessor.cs
@@ -0,0 +1,27 @@
+﻿using DemoLibrary.Implementations;
+
+namespace DemoLibrary.Abstractions;
+/// <summary>
+/// The Abstraction defines the interface for the "control" part of the two
+/// class hierarchies. It maintains a reference to an object of the
+/// Implementation hierarchy and delegates all of the real work to this
+/// object.
+/// This class defines the interface for the payment processing logic
+/// </summary>
+
+public class PaymentProcessor
+{
+    protected IPaymentGateway _paymentGateway;
+
+    public PaymentProcessor(IPaymentGateway paymentGateway)
+    {
+        _paymentGateway = paymentGateway;
+    }
+
+    public virtual void ProcessPayment(decimal amount, string data)
+    {
+        _paymentGateway.AuthorizePayment(amount, data);
+        _paymentGateway.CapturePayment();
+        Console.WriteLine("Payment process done!");
+    }
+}
