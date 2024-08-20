ff62b62136122ea1bea9f22e35a6c837ef18413a
Parsa <mehdipourparsa@gmail.com>
2024-08-20 19:26:11 -0400

    feat : add Client Code

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿// Client code
+using DemoLibrary.Abstractions;
+using DemoLibrary.ConcreteImplementations;
+using DemoLibrary.Implementations;
+using DemoLibrary.RefinedAbstractions;
+
+IPaymentGateway stripeGateway = new StripeGateway();
+PaymentProcessor creditCardProcessor = new CreditCardProcessor(stripeGateway);
+creditCardProcessor.ProcessPayment(100.00m, "4111111111111111");
+
+
+IPaymentGateway payPalGateway = new PayPalGateway();
+PaymentProcessor payPalProcessor = new PayPalProcessor(payPalGateway);
+payPalProcessor.ProcessPayment(50.00m, "example@email.com");
