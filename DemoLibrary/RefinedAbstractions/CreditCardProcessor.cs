7f552fa54a94070c04a902b93439d7f633c59c1a
Parsa <mehdipourparsa@gmail.com>
2024-08-20 19:26:03 -0400

    feat : add RefinedAbstractions

---
+++ DemoLibrary/RefinedAbstractions/CreditCardProcessor.cs
@@ -0,0 +1,21 @@
+﻿using DemoLibrary.Abstractions;
+using DemoLibrary.Implementations;
+
+namespace DemoLibrary.RefinedAbstractions;
+
+/// <summary>
+/// You can extend the Abstraction without changing the Implementation classes (in our example for different payment methods).
+/// </summary>
+public class CreditCardProcessor : PaymentProcessor
+{
+    public CreditCardProcessor(IPaymentGateway paymentGateway) : base(paymentGateway)
+    {
+    }
+
+    public override void ProcessPayment(decimal amount, string data)
+    {
+        base._paymentGateway.AuthorizePayment(amount, data);
+        base._paymentGateway.CapturePayment();
+        Console.WriteLine("Credit card payment process done!");
+    }
+}
