8000df41fe7f784b7255deb1f671dbf41301c16d
Parsa <mehdipourparsa@gmail.com>
2024-08-20 19:25:50 -0400

    feat : add ConcreteImplementations

---
+++ DemoLibrary/ConcreteImplementations/PayPalGateway.cs
@@ -0,0 +1,23 @@
+﻿using DemoLibrary.Implementations;
+
+namespace DemoLibrary.ConcreteImplementations;
+
+/// <summary>
+/// Each Concrete Implementation corresponds to a specific platform and
+/// implements the Implementation interface using that platform's API.
+/// The "Concrete Implementations" of the payment gateway
+/// </summary>
+public class PayPalGateway : IPaymentGateway
+{
+    public void AuthorizePayment(decimal amount, string data)
+    {
+        // Implement PayPal's authorization process for credit card payments
+        Console.WriteLine("PayPal : Approved");
+    }
+
+    public void CapturePayment()
+    {
+        // Implement PayPal's capture process
+        Console.WriteLine("PayPal : Captured");
+    }
+}
---
+++ DemoLibrary/ConcreteImplementations/StripeGateway.cs
@@ -0,0 +1,29 @@
+﻿using DemoLibrary.Implementations;
+
+namespace DemoLibrary.ConcreteImplementations;
+
+/// <summary>
+/// Each Concrete Implementation corresponds to a specific platform and
+/// implements the Implementation interface using that platform's API.
+/// The "Concrete Implementations" of the payment gateway
+/// </summary>
+public class StripeGateway : IPaymentGateway
+{
+    public void AuthorizePayment(decimal amount, string data)
+    {
+        // Implement Stripe's authorization process for credit card payments
+        Console.WriteLine("Stripe : Approved");
+    }
+
+    public void AuthorizePayment(decimal amount, string cardNumber, string expirationDate, string cvv)
+    {
+        // Implement Stripe's authorization process for credit card payments
+        Console.WriteLine("Stripe : Approved");
+    }
+
+    public void CapturePayment()
+    {
+        // Implement PayPal's capture process
+        Console.WriteLine("Stripe : Captured");
+    }
+}
